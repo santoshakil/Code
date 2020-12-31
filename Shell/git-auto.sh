1f213bb5891eae326a40c6dd4bb109dba6126751
Santo Shakil <shakil.mezbah@gmail.com>
2020-12-31 17:09:37 +0600

    1639: Test Commit

--- Shell/git-auto.sh
+++ Shell/git-auto.sh
@@ -1,8 +1,9 @@
 r=$RANDOM
 
-echo "$r: This is a test Commit for git">test-commit.txt
+echo "$r: This is a test Commit for git\n">>test-commit.txt
 echo "$r: This is a test Commit for git"
 
 git add .
 git commit -m "$r: Test Commit"
 git push origin master
+
