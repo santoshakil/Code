3edab47e7f566d4ef828a644232f78d2641a7463
Santo Shakil <shakil.mezbah@gmail.com>
2021-05-19 22:54:57 +0600

    ..

--- C/Practices/LightOj/chocolate_thief_1249.c
+++ C/Practices/LightOj/chocolate_thief_1249.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
+// Student object
 struct shanta
 {
+    // Name
     char s[105];
+    // volume = length * width * height
     long long a;
 };
 
 int main()
 {
-    int n, i, j, k, l, m, p;
+    int n, i, j, k, l, numberOfStudents, p;
     // number of test cases
     scanf("%d", &n);
     for (i = 1; i <= n; i++)
     {
         // number of students
-        scanf("%d", &m);
+        scanf("%d", &numberOfStudents);
         struct shanta name[200], temp;
         long long length[200], width[200], height[200], ck = 0;
-        // for each student
-        for (j = 0; j < m; j++)
+        // for each student scan students name, chocolate length, width, height
+        for (j = 0; j < numberOfStudents; j++)
         {
             scanf("%s %lld %lld %lld", name[j].s, &length[j], &width[j], &height[j]);
+            // calculate volume
             name[j].a = length[j] * width[j] * height[j];
         }
-        for (k = 0; k < m; k++)
+        for (k = 0; k < numberOfStudents; k++)
         {
-            for (l = 1 + k; l < m; l++)
+            for (l = 1 + k; l < numberOfStudents; l++)
             {
                 if (name[k].a < name[l].a)
                 {
@@ -41,12 +45,12 @@ int main()
                 }
             }
         }
-        for (p = 0; p < m - 1; p++)
+        for (p = 0; p < numberOfStudents - 1; p++)
         {
             if (name[p].a == name[p + 1].a)
                 ++ck;
         }
-        if (m - 1 == ck)
+        if (numberOfStudents - 1 == ck)
             printf("Case %d: no thief\n", i);
         else
             printf("Case %d: %s took chocolate from %s\n", i, name[0].s, name[p].s);
